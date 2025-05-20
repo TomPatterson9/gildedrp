21196855c5baaaccec9804b82e6c073d182c682d
TomPatterson9 <35941859+TomPatterson9@users.noreply.github.com>
2025-05-20 08:01:46 +0100

    maybe again

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
   output: 'export',
-  images: { unoptimized: true },
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
